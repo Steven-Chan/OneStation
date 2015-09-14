2b9e7d80b10d790be31e7e612b83a9312256c5ac
Steven-Chan <stevenchan@oursky.com>
2015-09-14 15:43:01 +0800

    removed spotify, included youtube search, and play result

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var ytdl = require('youtube-dl');
-var SpotifyWebApi = require('spotify-web-api-node');
+var Youtube = require('youtube-api');
+var config = require('./config.js');
 
-// var url = process.argv[2];
+var audio = null;
+
 var getYoutubeAudioLink = function (link, cb) {
   // var options = ['--username=', '--password='];
   var options = [];
@@ -19,39 +21,54 @@ var getYoutubeAudioLink = function (link, cb) {
   });
 }
 
-function runYouTube() {
+function runYouTube(id) {
   var t = document.getElementById('yt_audio_url');
-  getYoutubeAudioLink(t.value, function(url, err) {
-    // var a = document.getElementById('AudioPlayer');
-    // a.src = url;
-    // a.play();
-    var a = new Audio();
-    a.src = url;
-    a.play();
+  getYoutubeAudioLink(id || t.value, function(url, err) {
+    audio.src = url;
+    audio.play();
   });
 }
 
-function runSpotify() {
-  var t = document.getElementById('spotify_audio_url');
-  var s = document.getElementById('SpotifyPlayer');
-  s.src = "https://embed.spotify.com/?uri=spotify%3Atrack%3A" + t.value;
-  s.onload = function() {
-    console.log('onload');
-    s.onload = undefined;
-  }
-}
-
 function play () {
-  var s = document.getElementById('SpotifyPlayer');
-  // s.contentDocument.getElementsByClassName('clickable')[0].click();
-  s.contentDocument.getElementsByClassName('clickable')[0].click();
+  audio.play();
 }
 
 function pause () {
+  audio.pause();
+}
 
+function search () {
+  var s = document.getElementById('SearchBar');
+  var search = s.value;
+  s.value = '';
+
+  Youtube.search.list({
+    part: 'snippet',
+    maxResults: 5,
+    type: 'video',
+    fields: 'items(id,snippet)',
+    q: search
+  }, function(err, result) {
+    console.log(err, result);
+    if (err === null) {
+      var resultDiv = document.getElementById('SearchResult');
+      resultDiv.innerHTML = '';
+
+      for (var i = 0; i < result.items.length; i++) {
+        var v = result.items[i];
+        var div = document.createElement('div');
+        div.innerHTML = '<button onclick="runYouTube(\''+ v.id.videoId +'\')">play</button>' + v.snippet.title + ' - ' + v.id.videoId;
+
+        resultDiv.appendChild(div);
+      }
+    }
+  });
 }
 
 window.onload = function () {
-
+  audio = new Audio();
+  Youtube.authenticate({
+    type: 'key',
+    key: config['youtube-api-key']
+  });
 }
-
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
-// var SpotifyWebApi = require('spotify-web-api-node');
 
 require('crash-reporter').start();
 
@@ -36,26 +35,5 @@ app.on('ready', function() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
-  });
-
-  authWindow = new BrowserWindow({width: 800, height: 600});
-  authWindow.loadUrl('https://accounts.spotify.com/authorize?'+
-    'client_id=65d0bb0432a9429ba77734fdbf35e49e&'+
-    'redirect_uri=http:%2F%2Flocalhost%2Fcallback&'+
-    'scope=playlist-read-private&'+
-    'response_type=token');
-  authWindow.openDevTools();
-
-  authWindow.webContents.on('did-get-redirect-request', function(event, oldUrl, newUrl, isMainFrame) {
-    console.log('oldUrl ', oldUrl);
-    console.log('newUrl ', newUrl);
-    authWindow.close();
-  });
-
-  authWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    authWindow = null;
   });
 });
